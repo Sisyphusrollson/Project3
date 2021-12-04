7df01cd251986b8771ee8ced2aa2c7e83f8c6a4b
William <williamboysen1@gmail.com>
2021-12-03 22:43:47 -0600

    Massive Update, forgot to commit all these changes while working

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Control : MonoBehaviour
+{
+    [SerializeField] private Rigidbody playerRigidbody;
+    [SerializeField] private GameObject animatedObj;
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            playerRigidbody.AddForce(0, 1000, 0);
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            playerRigidbody.AddForce(1000, 0, 0);
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            playerRigidbody.AddForce(0, 0, 1000);
+        }
+        if(Input.GetKey("w"))
+        {
+            animatedObj.SetActive(true);
+            playerRigidbody.AddRelativeForce(Vector3.forward * 5);
+        }
+        else
+        {
+            animatedObj.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/CopyLimb.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyLimb : MonoBehaviour
+{
+    [SerializeField] private Transform targetLimb;
+
+    [SerializeField] private ConfigurableJoint m_configurableJoint;
+
+    Quaternion targetInitialRotation;
+    void Start()
+    {
+        this.m_configurableJoint = this.GetComponent<ConfigurableJoint>();
+        this.targetInitialRotation = this.targetLimb.transform.localRotation;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        this.m_configurableJoint.targetRotation = copyRotation();
+    }
+
+    private Quaternion copyRotation()
+    {
+        return Quaternion.Inverse(this.targetLimb.localRotation) * this.targetInitialRotation;
+    }
+}
